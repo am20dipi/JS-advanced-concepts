fc4e8ccd3ba3c47eea841cba55ce5cf6ab644a80
Adriana <am20dipi@alum.siena.edu>
2022-02-07 17:51:54 -0500

    Some prototype exercises with my solutions and the correct solutions

--- 2-PILLARS/Prototypes.js
+++ 2-PILLARS/Prototypes.js
@@ -1,4 +1,4 @@
-// Prototype Exercise
+// Prototype Exercise 1
 
 let human = {
     mortal: true
@@ -12,3 +12,48 @@ console.log(socrates.age) // => 75
 console.log(socrates.mortal) // => true
 console.log(human.isPrototypeOf(socrates)) // => true
 
+// Prototype Exercise 2
+
+// Given a Date Object, create a new method .lastYear()
+// which shows you the last year in 'YYYY' format
+
+// new Date('1900-10-10').lastYear()
+    // => should return '1899'
+
+
+   // My Solution
+const date = new Date('1900-10-10')
+function lastYear(date) {
+    return (date.getFullYear() - 1)
+}
+
+
+  // Correct Solution
+Date.prototype.lastYear = function () {
+    return this.getFullYear() - 1
+}
+new Date('1200-12-12').lastYear()
+
+
+
+// Prototype Exercise 3
+
+// Modify .map() to print 'hi' at the end of each item.
+// Ex: console.log([1,2,3].map()) => 1hi, 2hi, 3hi
+
+    // My Solution
+Array.prototype.map = function() {
+    return item + 'hi'
+}
+[1,2,3].map() // => 1,2,3hi
+
+    // Correct Solution
+Array.prototype.map = function() {
+    let arr = []
+    for (let i = 0; i < this.length; i++){
+        arr.push((this[i] + 'hi'))
+        
+    }
+    return arr
+}
+[1,2,3].map() // => 1,2,3hi
