50a30e5ab455c01c4ce02b1e5488930862c90f45
Adriana <am20dipi@alum.siena.edu>
2022-03-08 12:43:37 -0500

    Building out real world example of promises using fetch(), then(), catch() and Promise.all()

--- ASYNC/PromiseExample.js
+++ ASYNC/PromiseExample.js
@@ -20,3 +20,21 @@ oneSecondPromise.then(result => console.log(result))
 
 // output => Resolved after 1 second!
 // output => Promise {<fulfilled>: undefined}
+
+
+
+// Real World Example
+
+const urls = [
+    'https://jsonplaceholder.typicode.com/todos',
+    'https://jsonplaceholder.typicode.com/albums',
+    'https://jsonplaceholder.typicode.com/posts'
+]
+
+Promise.all(urls.map(url => {
+    return fetch(url).then(response => response.json())
+})).then(results => {
+    console.log(results[0])
+    console.log(results[1])
+    console.log(results[2])
+}).catch(() => console.log('error!'))
